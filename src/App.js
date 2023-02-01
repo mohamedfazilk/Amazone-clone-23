cc94aac1cf5f9ead0aaa5cae4f9c0997aa911483
mohamedfazilk <mohamedfazil961@gmail.com>
2023-02-01 23:04:05 +0530

    add the react router and start design the checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from 'react';
 import Header from './Components/Header/Header';
 import './App.css';
 import Home from './Components/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from './Components/Checkout/Checkout';
 
 function App() {
   return (
-    <div className="app">
-     <Header/>
-     <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+      <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />}/>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+        
+        
+        
+      </div>
+    </BrowserRouter>
+
   );
 }
 
